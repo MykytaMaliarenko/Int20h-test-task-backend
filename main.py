9c81051d20e46ef0f826130db4ba00416927c4b1
MykytaMaliarenko <mykytamaliarenko@gmail.com>
2020-01-19 20:45:09 +0200

    init

---
+++ main.py
@@ -0,0 +1,31 @@
+import json
+import os
+
+from flask import Flask
+from flask_restful import Api
+
+ENV_FILE = "env.json"
+
+app = Flask(__name__)
+api = Api(app)
+
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
+
+
+def init_env():
+    if os.path.isfile(ENV_FILE):
+        with open(ENV_FILE, "r") as file:
+            data = json.load(file)
+            for key in data:
+                os.environ[key] = data[key]
+
+
+if __name__ == '__main__':
+    init_env()
+    app.run(debug=True)
