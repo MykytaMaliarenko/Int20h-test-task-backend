274f1f670127705402fb500a9426159cba467b51
Abitgor <pozitivius228@gmail.com>
2020-01-23 18:32:41 +0200

    create get token func

--- external/deezer/deezer.py
+++ external/deezer/deezer.py
@@ -1,29 +1,45 @@
 import requests
-from flask import abort
 
 
-def create_search_request(artist, track):
-    try:
-        data = {
-            "q":{
-                "artist":artist,
-                "track":track
-            }
+def get_token(app_id, redirect_uri, secret):
+    data_for_authenticate = {
+        "app_id": app_id,
+        "redirect_uri": redirect_uri,
+        "perms": "basic_access,email"
+    }
+    url_for_authenticate = "https://connect.deezer.com/oauth/auth.php"
+    response = requests.head(url_for_authenticate, data_for_authenticate)
+    response = response.json()
+    data_for_getting_token = {
+        "app_id": app_id,
+        "secret": secret,
+        "code": response['code']
+    }
+    url_for_getting_token = "https://connect.deezer.com/oauth/access_token.php"
+    token = requests.get(url_for_getting_token, data_for_getting_token).json()
+    return token['access_token']
+
+
+def get_song_data(artist, track, token):
+    data = {
+        "access_token": token,
+        "q": {
+            "artist": artist,
+            "track": track
         }
-        response = requests.get("https://api.deezer.com/search", data)
-        if 'error' in response.json():
-            return response.json()['error']['message']
-        else:
-            resp = response.json()['data'][0]
-            return {
-                "song_name": resp['title'],
-                "album": resp['album']['title'],
-                "image": resp['album']['cover_medium'],
-                "duration": resp['duration'],
-                "rank": resp['rank'],
-                "album_id": resp['album']['id'],
-                "song_id": resp['id'],
-                "author": resp['artist']['name']
-            }
-    except:
-        return "error request is wrong"
+    }
+    response = requests.get("https://api.deezer.com/search", data)
+    if 'error' in response.json():
+        return response.json()['error']['message']
+    else:
+        response = response.json()['data'][0]
+        return {
+            "song_name": response['title'],
+            "album": response['album']['title'],
+            "image": response['album']['cover_medium'],
+            "duration": response['duration'],
+            "rank": response['rank'],
+            "album_id": response['album']['id'],
+            "song_id": response['id'],
+            "author": response['artist']['name']
+        }
